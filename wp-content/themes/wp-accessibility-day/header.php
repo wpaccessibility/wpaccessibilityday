314f1d64b408b5149855ddfc48da270f0bb2f0e8
Joe Dolson <design@joedolson.com>
2020-05-29 09:37:32 -0500

    Update header.php
    Since front page h1 is the name of the site, don't use H1 on front page logo.
    
    Also removes home link if currently viewing front page. (Don't link to self.)

--- wp-content/themes/wp-accessibility-day/header.php
+++ wp-content/themes/wp-accessibility-day/header.php
@@ -28,9 +28,9 @@
 				// get custom logo
 				$custom_logo_id = get_theme_mod( 'custom_logo' );
 				$custom_logo_attr = array( 'class' => 'custom-logo', 'alt' => '' );
-
+				// Since front page h1 is the name of the site, don't use H1 on front page logo.
 				if ( is_front_page() ) : ?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo wp_get_attachment_image( $custom_logo_id, 'full', false, $custom_logo_attr )?><span><?php bloginfo( 'name' ); ?></span></a></h1>
+					<p class="site-title"><?php echo wp_get_attachment_image( $custom_logo_id, 'full', false, $custom_logo_attr )?><span><?php bloginfo( 'name' ); ?></span></p>
 				<?php else : ?>
 					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo wp_get_attachment_image( $custom_logo_id, 'full', false, $custom_logo_attr )?><span><?php bloginfo( 'name' ); ?></span></a></p>
 				<?php
