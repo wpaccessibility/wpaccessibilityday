4194be86c47f6606a24912d574c364f7c2f69a7f
Joe Dolson <design@joedolson.com>
2020-09-18 13:05:24 -0500

    Internet Explorer timezone fallback

--- wp-content/themes/wp-accessibility-day/js/talk-time.js
+++ wp-content/themes/wp-accessibility-day/js/talk-time.js
@@ -5,6 +5,10 @@
 			var utcTime  = $( this ).attr( 'data-time' );
 			var userTime = new Date( utcTime ).toLocaleTimeString().replace( ':00 ', ' ' );
 			var zone     = Intl.DateTimeFormat().resolvedOptions().timeZone;
+			// Handle Internet Explorer's lack of timezone info.
+			if ( undefined === zone ) {
+				zone = 'your local time';
+			}
 			$( this ).append( '<span class="localtime">' + userTime + ' ' + zone + '</span>' );
 		});
 	});
