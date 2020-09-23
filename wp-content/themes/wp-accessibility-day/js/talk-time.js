4686e4a056208344097c70bb8703113b67714d86
Joe Dolson <design@joedolson.com>
2020-09-23 15:59:42 -0500

    Update talk-time to handle whole event date

--- wp-content/themes/wp-accessibility-day/js/talk-time.js
+++ wp-content/themes/wp-accessibility-day/js/talk-time.js
@@ -1,6 +1,17 @@
 (function ($) {
 	'use strict';
 	$(function () {
+		$( 'h2.event-time' ).each( function ( index ) {
+			var utcTime  = $( this ).attr( 'data-time' );
+			var userTime = new Date( utcTime ).toLocaleTimeString().replace( ':00 ', ' ' );
+			var userDate = new Date( utcTime ).toLocaleDateString();
+			var zone     = Intl.DateTimeFormat().resolvedOptions().timeZone;
+			// Handle Internet Explorer's lack of timezone info.
+			if ( undefined === zone ) {
+				zone = 'your local time';
+			}
+			$( this ).append( '<span class="localtime">' + userDate + ' at ' + userTime + ' ' + zone + '</span>' );
+		});
 		$( 'h2.talk-time' ).each( function ( index ) {
 			var utcTime  = $( this ).attr( 'data-time' );
 			var userTime = new Date( utcTime ).toLocaleTimeString().replace( ':00 ', ' ' );
