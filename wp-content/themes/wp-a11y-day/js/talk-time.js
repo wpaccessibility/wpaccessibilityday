2b73222d4d1de3d5a5b2d217dc53749fd2c79834
Joe Dolson <design@joedolson.com>
2022-09-27 19:00:45 -0500

    Add persistent show/hide toggle for chat & transcript

--- wp-content/themes/wp-a11y-day/js/talk-time.js
+++ wp-content/themes/wp-a11y-day/js/talk-time.js
@@ -55,5 +55,72 @@
 			console.log( content );
 		}
 
+		const streamtext = document.getElementById( 'streamtext' );
+		const transcript = document.getElementById( 'transcript' ).querySelector( 'button' );
+		const chat       = document.getElementById( 'chat' ).querySelector( 'button' );
+
+		if ( readCookie( 'streamtext.hidden' ) ) {
+			streamtext.classList.add( 'hidden' );
+			transcript.setAttribute( 'aria-expanded', 'true' );
+		}
+
+		if ( readCookie( 'slido.hidden' ) ) {
+			slido.classList.add( 'hidden' );
+			chat.setAttribute( 'aria-expanded', 'true' );
+		}
+
+		transcript.addEventListener( 'click', function() {
+			if ( streamtext.classList.contains( 'hidden' ) ) {
+				streamtext.classList.remove( 'hidden' );
+				transcript.setAttribute( 'aria-expanded', 'true' );
+				eraseCookie( 'streamtext.hidden' );
+			} else {
+				streamtext.classList.add( 'hidden' );
+				transcript.setAttribute( 'aria-expanded', 'false' );
+				createCookie( 'streamtext.hidden', 1 );
+			}
+		});
+
+		chat.addEventListener( 'click', function() {
+			if ( slido.classList.contains( 'hidden' ) ) {
+				slido.classList.remove( 'hidden' );
+				chat.setAttribute( 'aria-expanded', 'true' );
+				eraseCookie( 'slido.hidden' );
+			} else {
+				slido.classList.add( 'hidden' );
+				chat.setAttribute( 'aria-expanded', 'false' );
+				createCookie( 'slido.hidden', 1 );
+			}
+		});
+
+		// Cookie handler, non-$ style
+		function createCookie(name, value, days) {
+			if (days) {
+				var date = new Date();
+				date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+				var expires = "; expires=" + date.toGMTString();
+			} else {
+				var expires = '';
+			}
+
+			document.cookie = name + "=" + value + expires + "; path=/; SameSite=Strict;";
+		}
+
+		function readCookie(name) {
+			var nameEQ = name + "=";
+			var ca = document.cookie.split(';');
+			for (var i = 0; i < ca.length; i++) {
+				var c = ca[i];
+				while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+				if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+			}
+
+			return null;
+		}
+
+		function eraseCookie(name) {
+			createCookie(name, "");
+		}
+
 	});
 }(jQuery));
