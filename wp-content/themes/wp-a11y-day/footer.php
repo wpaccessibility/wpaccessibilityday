39e8cf87721d2bad73acc37825c6ad485c2e8df4
Amber Hinds <amberhinds@users.noreply.github.com>
2022-10-02 12:27:48 -0500

    add wrap and copyright

--- wp-content/themes/wp-a11y-day/footer.php
+++ wp-content/themes/wp-a11y-day/footer.php
@@ -12,25 +12,32 @@
 ?>
 
 <footer id="colophon" class="site-footer">
-	<h2 class="sr-only">Footer</h2>
+	<h2 class="sr-only">
+		Footer
+	</h2>
 	<?php
 		wp_accessibility_day_footer_sidebars();
 	?>
 	<div id="bottom-credits">
-		<div class="site-logo">
-			<?php 
-				echo do_shortcode('[logo]');
-			?>
-		</div>	
-		<nav id="footer-navigation" class="footer-navigation navigation" aria-label="Footer">
-			<?php
-				wp_nav_menu( array(
-					'theme_location' => 'menu-3',
-					'menu_id'        => 'footer-menu',
-					'depth'          => 1,
-				) );
-			?>
-		</nav><!-- #site-navigation -->
+		<div class="wrap">
+			<div class="site-logo">
+				<?php 
+					echo do_shortcode('[logo]');
+				?>
+				<p>
+					© 2022 WordPress Accessibility Day
+				</p>
+			</div>	
+			<nav id="footer-navigation" class="footer-navigation navigation" aria-label="Footer">
+				<?php
+					wp_nav_menu( array(
+						'theme_location' => 'menu-3',
+						'menu_id'        => 'footer-menu',
+						'depth'          => 1,
+					) );
+				?>
+			</nav><!-- #site-navigation -->
+		</div><!-- #wrap -->
 	</div><!-- #bottom-credits -->
 </footer><!-- #colophon -->
 </div><!-- #page -->
